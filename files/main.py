a82fa8356104b7ac447b8e30d3743f781d64cb36
LiZoMark841619 <githubs001@gmail.com>
2024-04-23 15:36:22 +0200

    ok

--- files/main.py
+++ files/main.py
@@ -8,8 +8,7 @@ def set_guesses() -> list:
     game_one = Guesses()
     game_one.set_num_of_players(get_valid_number('How many players are going to play? ', 1, 4))
     players = [Player() for _ in range(game_one.get_num_of_players())]
-    for player in players:
-        player.set_name(input('Enter your name: '))
+    for player in players: player.set_name(input('Enter your name: '))
     return players
 
 def guesses(func: object):
@@ -20,9 +19,9 @@ def guesses(func: object):
         count += 1
         for player in players:
             guess = get_valid_number(f'Enter your number {count} guess {player.get_name()}! ', 1, 100)
-            if guess < random_number: print('You number is less than I thought!\n '); continue
-            elif guess > random_number: print('Your number is greater than I thought!\n'); continue
-            else: print(f'You just hit, {player.get_name()} won. '); break
+            if guess < random_number: print('You number is less than I thought!\n ')
+            elif guess > random_number: print('Your number is greater than I thought!\n')
+            else: print(f'You just hit, {player.get_name()} won. '); return
                 
         quit_continue = get_valid_str('Enter q or c to quit or continue! ', 'q', 'c')
         if quit_continue == 'q': break
