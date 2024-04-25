7969e6d60a1024205e1b97a1c8f7aaab6d67f915
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 12:20:07 +0200

    ok

--- files/rock.py
+++ files/rock.py
@@ -1,12 +1,12 @@
 from games import Game
-
+from players import Player
 
 class Rock(Game):
     
     def settings(self):
         num_of_players = self.set_num_of_players(2)
         players = self.get_num_of_players()
-        for player in players: 
+        for player in players:
             player.set_name(input('Enter your name! '))
         return players
     
@@ -28,5 +28,3 @@ class Rock(Game):
             answers[0] == 'scissors' and answers[1] not in ['scissors', 'rock']
             else 'Even! Try again! ' if answers[0] == answers[1] 
             else f'{players[1].get_name()} you won! ')
-    
-    
