8811fac1ca4dae11f4a5c34ca2a0d3f8b5788b96
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 10:50:32 +0200

    ok

--- files/games.py
+++ files/games.py
@@ -1,6 +1,7 @@
 from players import Player
+from abc import ABC, abstractmethod
 
-class Game:
+class Game(ABC):
     
     def __str__(self) -> str:
         return f'Game: {self.__class__.__name__}'
@@ -10,3 +11,7 @@ class Game:
         
     def get_num_of_players(self) -> str:
         return [Player() for _ in range(self.nums)]
+    
+    @abstractmethod
+    def some_method(self):
+        pass
