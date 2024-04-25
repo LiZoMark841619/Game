62203916fc51a06c14da151383493cdb959ed29b
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 12:38:18 +0200

    ok

--- files/games.py
+++ files/games.py
@@ -3,23 +3,23 @@ from methods import get_valid_number
 from abc import ABC, abstractmethod
 
 class Game(ABC):
-    
+
     def __str__(self) -> str:
         return f'Game: {self.__class__.__name__}'
-    
+
     def set_num_of_players(self, nums: int) -> None:
         self.nums = nums
-        
+
     def get_num_of_players(self) -> str:
         return [Player() for _ in range(self.nums)]
-    
+
     def settings(self) -> str:
         num_of_players = self.set_num_of_players((get_valid_number('How many players are going to play? ', 1, 4)))
         players = self.get_num_of_players()
-        for player in players: 
+        for player in players:
             player.set_name(input('Enter your name! '))
         return players
-    
-    # @abstractmethod
-    # def some_method(self):
-    #     pass
+
+    @abstractmethod
+    def some_method(self):
+        pass
