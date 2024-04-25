3b7f7ba06ec3f8bd8d338589cbe869179e100bc2
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 15:36:55 +0200

    ok

--- files/rock.py
+++ files/rock.py
@@ -1,10 +1,11 @@
+import random
 from games import Game
 from methods import get_valid_number, get_valid_str
 
 class Rock(Game):
     
     def settings(self):
-        self.set_num_of_players(2)
+        self.set_num_of_players(1)
         players = self.get_num_of_players()
         for player in players: player.set_name(input('Enter your name! '))
         return players
@@ -16,14 +17,16 @@ class Rock(Game):
 
         while start < num_of_games:
             options = ['rock', 'paper', 'scissors']
+            computer = random.choice(options)
             answers = []
             for player in players:
                 question = get_valid_str(f'{player.get_name()} chose from {options}! ', *options)
                 answers.append(question)
+                answers.append(computer)
             start += 1
             print(f'{players[0].get_name()} you won! ' if (answers[0] == 'rock' and answers[1] not in ['rock', 'paper']) or 
             (answers[0] == 'paper' and answers[1] not in ['paper', 'scissors'])or answers[0] == 'scissors' and answers[1] not in ['scissors', 'rock']
-            else 'Even! Try again! ' if answers[0] == answers[1] else f'{players[1].get_name()} you won! ')
+            else 'Even! Try again! ' if answers[0] == answers[1] else 'Computer won! ')
     
     def some_method(self):
         pass
