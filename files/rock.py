d957ffedfae7b9dea70a2a9700802552213a50fd
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 12:18:26 +0200

    ok

---
+++ files/rock.py
@@ -0,0 +1,32 @@
+from games import Game
+
+
+class Rock(Game):
+    
+    def settings(self):
+        num_of_players = self.set_num_of_players(2)
+        players = self.get_num_of_players()
+        for player in players: 
+            player.set_name(input('Enter your name! '))
+        return players
+    
+    def play(self):
+        start = 0
+        num_of_games = get_valid_number('Enter the number of games you would like to play (1-5)! ', 1, 5)
+
+        while start < num_of_games:
+            options = ['rock', 'paper', 'scissors']
+            answers = []
+            for player in players:
+                question = get_valid_str(f'{player.get_name()} chose from {options}', *options)
+                answers.append(question)
+            start += 1
+            
+            print(f'{players[0].get_name()} you won! ' 
+            if (answers[0] == 'rock' and answers[1] not in ['rock', 'paper']) or 
+            (answers[0] == 'paper' and answers[1] not in ['paper', 'rock'])or
+            answers[0] == 'scissors' and answers[1] not in ['scissors', 'rock']
+            else 'Even! Try again! ' if answers[0] == answers[1] 
+            else f'{players[1].get_name()} you won! ')
+    
+    
