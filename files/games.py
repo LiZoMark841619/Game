a8c7fed7efb226fc6fcb73e0bf55a8fcad39926c
LiZoMark841619 <githubs001@gmail.com>
2024-04-25 12:26:36 +0200

    ok

--- files/games.py
+++ files/games.py
@@ -6,8 +6,8 @@ class Game(ABC):
     def __str__(self) -> str:
         return f'Game: {self.__class__.__name__}'
     
-    def set_num_of_players(self, numbers: int) -> None:
-        self.nums = numbers
+    def set_num_of_players(self, nums: int) -> None:
+        self.nums = nums
         
     def get_num_of_players(self) -> str:
         return [Player() for _ in range(self.nums)]
