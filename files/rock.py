884c7abef0526e9559b854023a80a42c09312663
LiZoMark841619 <githubs001@gmail.com>
2024-04-26 17:51:18 +0200

    ok

--- files/rock.py
+++ files/rock.py
@@ -1,32 +1,29 @@
 import random
 from games import Game
+from players import Player
 from methods import get_valid_number, get_valid_str
 
 class Rock(Game):
     
     def settings(self) -> list:
         self.set_num_of_players(1)
-        players = self.make_players()
-        for player in players: player.set_name(input('Enter your name! '))
-        return players
+        player = Player()
+        player.set_name(input('Enter your name! '))
+        return player
 
     def play(self) -> str:
-        players = self.settings()
+        player = self.settings()
         start = 0
         num_of_games = get_valid_number('Enter the number of games you would like to play (1-5)! ', 1, 5)
 
         while start < num_of_games:
             options = ['rock', 'paper', 'scissors']
-            computer_num = random.choice(options)
-            answers = []
-            for player in players:
-                question = get_valid_str(f'{player.get_name()} chose from {options}! ', *options)
-                answers.append(question)
-                answers.append(computer_num)
+            comp_answ, answs = random.choice(options), []
+            question = get_valid_str(f'{player.get_name()} chose from {options}! ', *options)
+            answs.extend([question, comp_answ])
             start += 1
-            print(f'{players[0].get_name()} you won, Computer chose {computer_num}! ' if (answers[0] == 'rock' and answers[1] not in ['rock', 'paper']) or 
-            (answers[0] == 'paper' and answers[1] not in ['paper', 'scissors'])or answers[0] == 'scissors' and answers[1] not in ['scissors', 'rock']
-            else f'Even! Computer chose {computer_num}. Try again! ' if answers[0] == answers[1] else f'{players[0].get_name()} lost, Computer chose {computer_num}.')
+            print(f'{player.get_name()} you won, Computer chose {comp_answ}! ' if (answs[0] == 'rock' and answs[1] not in ['rock', 'paper']) or 
+            (answs[0] == 'paper' and answs[1] not in ['paper', 'scissors'])or answs[0] == 'scissors' and answs[1] not in ['scissors', 'rock']
+            else f'Even! Computer chose {comp_answ}. Try again! ' if answs[0] == answs[1] else f'{player.get_name()} lost, Computer chose {comp_answ}.')
     
-    def some_method(self):
-        pass
+    def some_method(self): pass
