158a9d28155d978679503dcbec17729a28f03a4f
LiZoMark841619 <githubs001@gmail.com>
2024-04-28 19:56:11 +0200

    ok

--- files/menu.py
+++ files/menu.py
@@ -7,9 +7,9 @@ class Menu(Valids):
         return f'Menu({self.__class__.__name__})'
     
     def addItem(self, request: str, opt_1: Any, opt_2: Any):
-        if (type(opt_1) and type(opt_2))!= int:
+        if (type(opt_1) and type(opt_2))!= int or float:
             self.request = self.get_valid_str(request, opt_1, opt_2)
-        elif (type(opt_1) and type(opt_2))!= str: 
+        elif (type(opt_1) and type(opt_2))!= str or float: 
             self.request = self.get_valid_number(request, opt_1, opt_2)
     
     def render(self):
