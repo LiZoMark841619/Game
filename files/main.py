0486cb4ad66050e3c4b6060ee49b98486feeb753
LiZoMark841619 <githubs001@gmail.com>
2024-04-28 22:33:56 +0200

    ok

--- files/main.py
+++ files/main.py
@@ -5,24 +5,15 @@ from menu import Menu
 from menu_game import GameMenu
 
 while True:
-    
     menu = GameMenu()
     if not menu.ask_to_play(): break
-    else:
-        game = menu.chose_game()
-
-        if game == 'guesses':
-            Guesses().play()
-            
-        elif game == 'lotto':
-            Lotto().play()
-
-        elif game == 'rock':
-            Rock().play()
+    
+    game = menu.chose_game()
+    if game == 'guesses': Guesses().play()
+    elif game == 'lotto': Lotto().play()
+    elif game == 'rock': Rock().play()
 
     good_bye = Menu()
     good_bye.addItem('Would you like to quit? Enter yes or no! ', 'yes', 'no')
-    if good_bye.render() == 'yes':
-        print('Thank you for your time! Good bye and have a nice day!'); break
-    else:
-        print('Ok, let us restart! '); continue
+    if good_bye.render() == 'yes': print('Thank you for your time! Good bye and have a nice day!'); break
+    else: print('Ok, let us restart! '); continue
