91741882b75226c18e9377fd3de674a1fe300b98
LiZoMark841619 <githubs001@gmail.com>
2024-04-28 19:51:54 +0200

    ok

--- files/menu.py
+++ files/menu.py
@@ -1,5 +1,15 @@
 from valids import Valids
+from typing import Any
 
 class Menu(Valids):
     
     def __init__(self) -> None: pass
+    
+    def addItem(self, request: str, opt_1: Any, opt_2: Any):
+        if (type(opt_1) and type(opt_2))!= int:
+            self.request = self.get_valid_str(request, opt_1, opt_2)
+        elif (type(opt_1) and type(opt_2))!= str: 
+            self.request = self.get_valid_number(request, opt_1, opt_2)
+    
+    def render(self):
+        return self.request
