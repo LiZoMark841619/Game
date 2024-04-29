9d7460e23be6493e84a31daeb6e0b092d4522825
LiZoMark841619 <githubs001@gmail.com>
2024-04-29 21:44:28 +0200

    ok

--- files/guesses.py
+++ files/guesses.py
@@ -3,12 +3,12 @@ from games import Game
 
 class Guesses(Game):
     
-    def game_set(self):
-        set_num_players = self.set_num_of_players(self.get_valid_number('How many players are going to play? Set from 1 to 4! ', 1, 4))
+    def game_set(self) -> tuple:
+        self.set_num_of_players(self.get_valid_number('How many players are going to play? Set from 1 to 4! ', 1, 4))
         players = self.set_players_names()
         return players, random.randint(1, 100), 0
         
-    def play(self):
+    def play(self) -> None:
         players, random_number, count = self.game_set()
         print('I thought of a number from 1 to 100! Find out what is it! ')
         while True:
--- files/menu.py
+++ files/menu.py
@@ -6,7 +6,7 @@ class Menu(Valid):
     def __repr__(self) -> str:
         return f'Menu({self.__class__.__name__})'
     
-    def addItem(self, request: str, opt_1: Any, opt_2: Any)-> None :
+    def addItem(self, request: str, opt_1: Any, opt_2: Any) -> None :
         self.request = self.get_valid_str(request, opt_1, opt_2)
     
     def render(self) -> str:
