e3492a313497982e6c0b6812e953a5b24ffa0884
LiZoMark841619 <githubs001@gmail.com>
2024-05-01 19:05:53 +0200

    ok

--- files/menu_game.py
+++ files/menu_game.py
@@ -5,7 +5,7 @@ class GameMenu(Menu):
     
     def ask_to_play(self) -> bool:
         if self.get_valid_string('Would you like to play? Enter yes or no? ', 'yes', 'no') == 'yes': return True
-        print("Understood. Good bye!"); return False
+        print("Understood. Good bye!")
     
     def choose_game(self) -> str:
         game = self.get_valid_string(f'Choose from {[cls.__name__ for cls in Game.__subclasses__()]}! ', 'guesses', 'lotto', 'rock')
