eff1c3e44c922ab6b98622ee6062d46931a8a3fa
LiZoMark841619 <githubs001@gmail.com>
2024-05-05 23:08:56 +0200

    ok

--- files/primes.py
+++ files/primes.py
@@ -1,3 +1,4 @@
+from players import Player
 from games import Game
 from menu import Menu
 import array
@@ -25,12 +26,16 @@ class Primes(Game):
         primes, not_primes = self.store_primes(n), self.store_not_primes(n)
         yield from (num for num in not_primes for i in range(len(primes)) for k in range(i+1) if num == primes[i] * primes[k])
 
+    def game_set(self):
+        player = Player()
+        player.set_name(input('Enter your name!'))
+        return player
+    
     def play(self):
-        self.set_num_of_players(1)
-        self.set_players_names()
+        player = self.game_set()
         num = self.get_valid_number('Enter a number from 2 to 500000! ', 2, 500000)
         menu = Menu()
-        menu.add_item('Chose from [primes, factors, semi-primes] options to list the numbers! ', 'primes', 'factors', 'semi-primes')
+        menu.add_item(f'{player.get_name()}, chose from [primes, factors, semi-primes] to list numbers! ', 'primes', 'factors', 'semi-primes')
         if menu.render() == 'primes': print(*self.store_primes(num))
         elif menu.render() == 'factors': print(*self.store_prime_factors(num))
         else: print(list(self.collect_semi_primes(num)))
