cce2665430c7f281a7e3c3be37e597971c39ccda
LiZoMark841619 <githubs001@gmail.com>
2024-07-07 21:37:38 +0200

    ok

--- files/valids.py
+++ files/valids.py
@@ -3,14 +3,16 @@ class InstanceTree:
         return f'Object created from {self.__class__.__name__} class with attributes: {self.attrs_()}'
     
     def attrs_(self):
-        for attr in self.__dict__: print(attr, '->', self.__dict__[attr])
+        for attr in self.__dict__: 
+            print(attr, '->', self.__dict__[attr])
             
 class Valid(InstanceTree):
     def get_valid_number(self, prompt: str, value_min: int, value_max: int) -> int:
         while True:
             try: 
                 value = int(input(prompt))
-                if value_min <= value <= value_max: return value
+                if value_min <= value <= value_max: 
+                    return value
                 print('The number is out of range. Try again!\n')
             except ValueError: print('Invalid value. Try again!\n')
 
